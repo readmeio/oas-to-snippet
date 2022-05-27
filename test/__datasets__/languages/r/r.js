284eb98088e9ce6e1db42c20f5520602433ed7f2
Jon Ursenbach <erunion@users.noreply.github.com>
2022-05-26 18:33:07 -0700

    fix: upgrade httpsnippet to fix a bug with r snippets building querystrings wrong (#93)

--- test/__datasets__/languages/r/r.js
+++ test/__datasets__/languages/r/r.js
@@ -4,8 +4,8 @@ module.exports =
   'url <- "http://petstore.swagger.io/v2/user/login"\n' +
   '\n' +
   'queryString <- list(\n' +
-  '  username = "woof"\n' +
-  '  password = "barkbarkbark",\n' +
+  '  username = "woof",\n' +
+  '  password = "barkbarkbark"\n' +
   ')\n' +
   '\n' +
   'response <- VERB("GET", url, query = queryString, content_type("application/octet-stream"), accept("application/xml"))\n' +
