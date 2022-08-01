9b868847b9e40fb28a7e54b989bb503c594733ac
Jon Ursenbach <erunion@users.noreply.github.com>
2022-08-01 12:14:39 -0700

    feat: upgrading httpsnippet-client-api to have headers in api snippets lowercased (#113)

--- test/__datasets__/languages/node/api.js
+++ test/__datasets__/languages/node/api.js
@@ -1,5 +1,5 @@
 module.exports = `const sdk = require('api')('https://example.com/openapi.json');
 
-sdk.loginUser({username: 'woof', password: 'barkbarkbark', Accept: 'application/xml'})
+sdk.loginUser({username: 'woof', password: 'barkbarkbark', accept: 'application/xml'})
   .then(res => console.log(res))
   .catch(err => console.error(err));`;
