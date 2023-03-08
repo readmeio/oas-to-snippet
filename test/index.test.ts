5d15bf30657670b35a4d1e45d5ab30e699b56ac3
Jon Ursenbach <jon@ursenba.ch>
2023-03-07 17:00:23 -0800

    test: moving unit testing back to jest

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,9 +4,7 @@ import type { SupportedLanguages } from 'supportedLanguages';
 import fileUploads from '@readme/oas-examples/3.0/json/file-uploads.json';
 import petstoreOas from '@readme/oas-examples/3.0/json/petstore.json';
 import * as extensions from '@readme/oas-extensions';
-import chai, { expect } from 'chai';
 import harExamples from 'har-examples';
-import { jestSnapshotPlugin } from 'mocha-chai-jest-snapshot';
 import Oas from 'oas';
 
 import { oasToSnippet, supportedLanguages } from '../src';
@@ -14,8 +12,6 @@ import { oasToSnippet, supportedLanguages } from '../src';
 import owlbertShrub from './__datasets__/owlbert-shrub.dataurl.json';
 import owlbert from './__datasets__/owlbert.dataurl.json';
 import queryEncodedHAR from './__datasets__/query-encoded.har.json';
-
-chai.use(jestSnapshotPlugin());
 
 const petstore = Oas.init(petstoreOas);
 
@@ -26,7 +22,7 @@ describe('oas-to-snippet', function () {
   describe('HAR overrides', function () {
     it('should be able to accept a har override', function () {
       const { code } = oasToSnippet(null, null, null, null, 'node', null, harExamples.full);
-      expect(code).to.equal(`const { URLSearchParams } = require('url');
+      expect(code).toBe(`const { URLSearchParams } = require('url');
 const fetch = require('node-fetch');
 const encodedParams = new URLSearchParams();
 
@@ -52,7 +48,7 @@ fetch(url, options)
     it('should treat overrides as if they are not yet encoded', function () {
       const { code } = oasToSnippet(null, null, null, null, 'node', null, queryEncodedHAR as unknown as HarRequest);
 
-      expect(code).to.equal(`const fetch = require('node-fetch');
+      expect(code).toBe(`const fetch = require('node-fetch');
 
 const url = 'https://httpbin.org/anything?startTime=2019-06-13T19%3A08%3A25.455Z&endTime=2015-09-15T14%3A00%3A12-04%3A00';
 const options = {method: 'GET'};
@@ -68,7 +64,7 @@ fetch(url, options)
     // @ts-expect-error Testing an improper typing case here.
     const codeSnippet = oasToSnippet(petstore, petstore.operation('/pet/{petId}', 'get'), {}, {}, 'css', oasUrl);
 
-    expect(codeSnippet).to.deep.equal({
+    expect(codeSnippet).toStrictEqual({
       code: '',
       highlightMode: false,
     });
@@ -77,7 +73,7 @@ fetch(url, options)
   it('should pass through values to code snippet', function () {
     const { code } = oasToSnippet(petstore, petstore.operation('/pet/{petId}', 'get'), formData, {}, 'node', oasUrl);
 
-    expect(code).to.contain('http://petstore.swagger.io/v2/pet/123');
+    expect(code).toContain('http://petstore.swagger.io/v2/pet/123');
   });
 
   it('should pass through json values to code snippet', function () {
@@ -90,7 +86,7 @@ fetch(url, options)
       oasUrl
     );
 
-    expect(code).to.contain("body: JSON.stringify({id: '123'}");
+    expect(code).toContain("body: JSON.stringify({id: '123'}");
   });
 
   it('should pass through form encoded values to code snippet', function () {
@@ -109,9 +105,9 @@ fetch(url, options)
       oasUrl
     );
 
-    expect(code).to.contain("encodedParams.set('id', '123');");
-    expect(code).to.contain("encodedParams.set('name', 'buster');");
-    expect(code).to.contain('body: encodedParams');
+    expect(code).toContain("encodedParams.set('id', '123');");
+    expect(code).toContain("encodedParams.set('name', 'buster');");
+    expect(code).toContain('body: encodedParams');
   });
 
   it('should have special indents for curl snippets', function () {
@@ -147,7 +143,7 @@ fetch(url, options)
       'curl'
     );
 
-    expect(code).to.equal(`curl --request GET \\
+    expect(code).toBe(`curl --request GET \\
      --url https://example.com/body \\
      --header 'content-type: application/x-www-form-urlencoded' \\
      --data a=test \\
@@ -162,20 +158,20 @@ fetch(url, options)
 
     const { code } = oasToSnippet(oas, oas.operation('/pet/{petId}', 'post'), formData, {}, 'node', oasUrl);
 
-    expect(code).to.contain('http://petstore.swagger.io/v2/pet/123');
-    expect(code).not.to.contain('try.readme.io');
+    expect(code).toContain('http://petstore.swagger.io/v2/pet/123');
+    expect(code).not.toContain('try.readme.io');
   });
 
   it('should not contain `withCredentials` in javascript snippets', function () {
     const { code } = oasToSnippet(petstore, petstore.operation('/pet/{petId}', 'post'), {}, {}, 'javascript', oasUrl);
 
-    expect(code).not.to.match(/withCredentials/);
+    expect(code).not.toMatch(/withCredentials/);
   });
 
   it('should return with unhighlighted code', function () {
     const { code } = oasToSnippet(petstore, petstore.operation('/pet/{petId}', 'post'), {}, {}, 'javascript', oasUrl);
 
-    expect(code).not.to.match(/cm-s-tomorrow-night/);
+    expect(code).not.toMatch(/cm-s-tomorrow-night/);
   });
 
   describe('multipart/form-data handlings', function () {
@@ -251,7 +247,7 @@ fetch(url, options)
         oasUrl
       );
 
-      expect(code).to.equal(`curl --request POST \\
+      expect(code).toBe(`curl --request POST \\
      --url https://example.com/multipart \\
      --header 'content-type: multipart/form-data' \\
      --form orderId=10 \\
@@ -302,10 +298,10 @@ fetch(url, options)
       oasUrl
     );
 
-    expect(snippet.code).to.contain(encodeURIComponent(startTime));
-    expect(snippet.code).to.contain(encodeURIComponent(endTime));
-    expect(snippet.code).not.to.contain(encodeURIComponent(encodeURIComponent(startTime)));
-    expect(snippet.code).not.to.contain(encodeURIComponent(encodeURIComponent(endTime)));
+    expect(snippet.code).toContain(encodeURIComponent(startTime));
+    expect(snippet.code).toContain(encodeURIComponent(endTime));
+    expect(snippet.code).not.toContain(encodeURIComponent(encodeURIComponent(startTime)));
+    expect(snippet.code).not.toContain(encodeURIComponent(encodeURIComponent(endTime)));
   });
 
   it('should handle `multipart/form-data` payloads of multiple files', function () {
@@ -319,8 +315,8 @@ fetch(url, options)
       'node'
     );
 
-    expect(snippet.code).to.contain("formData.append('filename', fs.createReadStream('owlbert.png'));");
-    expect(snippet.code).to.contain("formData.append('filename', fs.createReadStream('owlbert-shrub.png'));");
+    expect(snippet.code).toContain("formData.append('filename', fs.createReadStream('owlbert.png'));");
+    expect(snippet.code).toContain("formData.append('filename', fs.createReadStream('owlbert-shrub.png'));");
   });
 
   it('should handle a `multipart/form-data` payload where a file has an underscore in its name', function () {
@@ -338,46 +334,45 @@ fetch(url, options)
       'node'
     );
 
-    expect(snippet.code).to.contain("formData.append('documentFile', fs.createReadStream('lorem_ipsum.txt'));");
+    expect(snippet.code).toContain("formData.append('documentFile', fs.createReadStream('lorem_ipsum.txt'));");
   });
 
   describe('supported languages', function () {
-    // eslint-disable-next-line mocha/no-setup-in-describe
     Object.keys(supportedLanguages).forEach((lang: keyof SupportedLanguages) => {
-      describe(lang, function () {
-        // eslint-disable-next-line mocha/no-setup-in-describe
+      describe(`${lang}`, function () {
         const targets = Object.keys(supportedLanguages[lang].httpsnippet.targets);
 
         it('should have a language definition', function () {
-          expect(supportedLanguages[lang].highlight).to.be.a('string');
-          expect(supportedLanguages[lang].httpsnippet.lang).to.be.a('string');
-          expect(supportedLanguages[lang].httpsnippet.default).to.be.a('string');
-          expect(supportedLanguages[lang].httpsnippet.targets).to.be.a('object');
+          expect(supportedLanguages[lang].highlight).toStrictEqual(expect.any(String));
+          expect(supportedLanguages[lang].httpsnippet.lang).toStrictEqual(expect.any(String));
+          expect(supportedLanguages[lang].httpsnippet.default).toStrictEqual(expect.any(String));
+          expect(supportedLanguages[lang].httpsnippet.targets).toStrictEqual(expect.any(Object));
 
-          expect(targets.length).to.be.at.least(1);
-          expect(targets).to.contain(supportedLanguages[lang].httpsnippet.default);
+          expect(targets.length).toBeGreaterThanOrEqual(1);
+          expect(targets).toContain(supportedLanguages[lang].httpsnippet.default);
         });
 
         it('should generate code for the default target', function () {
           const snippet = oasToSnippet(petstore, petstore.operation('/pet', 'post'), formData, {}, lang);
 
           expect(snippet.code).toMatchSnapshot();
-          expect(snippet.highlightMode).to.equal(supportedLanguages[lang].highlight);
+          expect(snippet.highlightMode).toBe(supportedLanguages[lang].highlight);
         });
 
         describe('targets', function () {
-          // eslint-disable-next-line mocha/no-setup-in-describe
           targets.forEach(target => {
-            describe(target, function () {
+            describe(`${target}`, function () {
               it('should be properly defined', function () {
-                expect(supportedLanguages[lang].httpsnippet.targets[target].name).to.be.a('string');
+                expect(supportedLanguages[lang].httpsnippet.targets[target].name).toStrictEqual(expect.any(String));
 
                 if ('opts' in supportedLanguages[lang].httpsnippet.targets[target]) {
-                  expect(supportedLanguages[lang].httpsnippet.targets[target].opts).to.be.a('object');
+                  expect(supportedLanguages[lang].httpsnippet.targets[target].opts).toStrictEqual(expect.any(Object));
                 }
 
                 if ('install' in supportedLanguages[lang].httpsnippet.targets[target]) {
-                  expect(supportedLanguages[lang].httpsnippet.targets[target].install).to.be.a('string');
+                  expect(supportedLanguages[lang].httpsnippet.targets[target].install).toStrictEqual(
+                    expect.any(String)
+                  );
                 }
               });
 
@@ -394,7 +389,7 @@ fetch(url, options)
                 );
 
                 expect(snippet.code).toMatchSnapshot();
-                expect(snippet.highlightMode).to.equal(supportedLanguages[lang].highlight);
+                expect(snippet.highlightMode).toBe(supportedLanguages[lang].highlight);
               });
             });
           });
@@ -403,7 +398,6 @@ fetch(url, options)
     });
 
     describe('backwards compatibiltiy', function () {
-      // eslint-disable-next-line mocha/no-setup-in-describe
       ['curl', 'node-simple'].forEach((lang: 'curl' | 'node-simple') => {
         it(`should still support \`${lang}\` as the supplied language`, function () {
           const snippet = oasToSnippet(
@@ -420,9 +414,9 @@ fetch(url, options)
           expect(snippet.code).toMatchSnapshot();
 
           if (lang === 'curl') {
-            expect(snippet.highlightMode).to.equal('shell');
+            expect(snippet.highlightMode).toBe('shell');
           } else if (lang === 'node-simple') {
-            expect(snippet.highlightMode).to.equal('javascript');
+            expect(snippet.highlightMode).toBe('javascript');
           }
         });
       });
@@ -433,8 +427,8 @@ fetch(url, options)
       // passing in an API definition for it to look or an operation in.
       const snippet = oasToSnippet(null, null, null, null, 'node-simple', oasUrl, harExamples.full);
 
-      expect(snippet.code).to.contain('node-fetch');
-      expect(snippet.highlightMode).to.equal('javascript');
+      expect(snippet.code).toContain('node-fetch');
+      expect(snippet.highlightMode).toBe('javascript');
     });
   });
 });
